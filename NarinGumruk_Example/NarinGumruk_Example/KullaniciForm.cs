e89ad586f0a901b1b75b16b032854e60f84eada2
SemihVRL <143089186+SemihVRL@users.noreply.github.com>
2024-06-13 01:28:01 +0300

    WinForm_Example

---
+++ NarinGumruk_Example/NarinGumruk_Example/KullaniciForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+
+namespace NarinGumruk_Example
+{
+    public partial class KullaniciForm : Form
+    {
+        public KullaniciForm()
+        {
+            InitializeComponent();
+
+        }
+        Narin_Gumruk_ExampleProjectEntities example = new Narin_Gumruk_ExampleProjectEntities();
+        private string sehir;
+
+        private void KullaniciForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = example.KullaniciExample_TBL.ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            if (e.ColumnIndex == 3)
+            {
+
+                //string IndexValue = dataGridView1.Rows[e.ColumnIndex].Cells[e.RowIndex].Value.ToString();
+
+                SehirForm sehirForm = new SehirForm();
+                sehirForm.SecilenSehir += sehirForm_SecilenSehir;
+                sehirForm.Show();
+            }
+
+
+        }
+
+        private void sehirForm_SecilenSehir(string StringSehir)
+        {
+            dataGridView1.CurrentRow.Cells[3].Value = StringSehir;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+               
+                if (row.Cells["DogumYeri"].Value != null && row.Cells["DogumYeri"].Value != DBNull.Value)
+                {
+                    string sehir = row.Cells["DogumYeri"].Value.ToString();
+
+                    KullaniciExample_TBL kullanici = new KullaniciExample_TBL
+                    {
+                        DogumYeri = sehir,
+                    };
+
+                    example.KullaniciExample_TBL.Add(kullanici);
+                }
+            }
+
+            example.SaveChanges();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           dataGridView1.DataSource=example.KullaniciExample_TBL.ToList();
+        }
+    }
+}
---
+++ NarinGumruk_Example/NarinGumruk_Example/SehirForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net.Http.Headers;
+
+namespace NarinGumruk_Example
+{
+    public partial class SehirForm : Form
+    {
+        public event Action<string> SecilenSehir;
+        public SehirForm()
+        {
+            InitializeComponent();
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+        Narin_Gumruk_ExampleProjectEntities example= new Narin_Gumruk_ExampleProjectEntities();
+        private void SehirForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource= example.Sehir_TBL.ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex>=0)
+            {
+                string StringSehir = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+               SecilenSehir?.Invoke(StringSehir);
+
+                
+            }
+        }
+    }
+}
